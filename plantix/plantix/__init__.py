b735c4544d7b268d4d5203ba12960575a2a6c8d9
carlosghabrous <carlos.ghabrous@gmail.com>
2021-09-22 16:41:25 +0200

    Add __init__.py, setup.py and finish tests.

--- plantix/plantix/__init__.py
+++ plantix/plantix/__init__.py
@@ -1,1 +1,3 @@
+from .sdk import PlantixApiClient
+
 __version__ = "0.0.1"
--- plantix/plantix/tests/test_basic.py
+++ plantix/plantix/tests/test_basic.py
@@ -1,16 +1,33 @@
 import plantix 
 
-from collections import Counter
+from urllib.error import HTTPError
+
+# from collections import Counter
 
 def test_version_can_be_read():
     '''This test is just to make sure the packaging is OK'''
     assert plantix.__version__ is not None
 
-def test_visit_connections_returns_empty_ranked_topics_if_no_connections_given():
-    connections, visited, ranked_topics = list(), dict(), Counter()
+def test_plantix_expert_is_correctly_built():
+    try: 
+        result_0  = plantix.PlantixApiClient().get('0')
+        
+        assert result_0.uid is not None
+        assert result_0.topics is not None and isinstance(result_0.topics, list)
+        assert result_0.following is not None and isinstance(result_0.following, list)
 
-    plantix.sdk.find_topics(connections, visited, ranked_topics)
-    assert ranked_topics == Counter() 
+    except HTTPError: 
+        pass
 
-def test_user_uid_0_most_common_topic():
-    pass 
+def test_find_topics_works_ok_for_user_0():
+    try: 
+        topics_0 = plantix.PlantixApiClient().find_topics('0', 1)
+
+        assert len(topics_0) == 1 
+        crop, occurrences = topics_0[0][0], topics_0[0][1]
+        assert crop == 'cucumber'
+        assert occurrences == 5
+        
+    except HTTPError:
+        pass
+
